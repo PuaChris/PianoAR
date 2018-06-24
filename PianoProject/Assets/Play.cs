90659620ab1ca4139812bfc50edc42d5f7daf7be
PuaChris <chris.pua@mail.utoronto.ca>
2018-06-24 10:44:36 -0400

    Cleaned up code

--- PianoProject/Assets/Play.cs
+++ PianoProject/Assets/Play.cs
@@ -6,17 +6,16 @@ using UnityEngine.UI;
 public class Play : MonoBehaviour {
 
     public Button play_button;
-
         
     // Use this for initialization
 	void Start () {
         play_button = GameObject.Find("Play").GetComponent<Button>();
+        play_button.onClick.AddListener(PlaySong);
     }
 
     // Update is called once per frame
     void Update () {
         play_button = GameObject.Find("Play").GetComponent<Button>();
-        play_button.onClick.AddListener(PlaySong);
     }
 
     void PlaySong()
